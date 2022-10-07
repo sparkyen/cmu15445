be412c3daf3fc5d6845d098542df747b0ea84761
sparkyen <sparkyen@qq.com>
2022-10-06 21:28:54 -0700

    pass test about P3:TASK #1-SYSTEM CATALOG for now

--- bustub_2020/src/include/catalog/catalog.h
+++ bustub_2020/src/include/catalog/catalog.h
@@ -77,14 +77,34 @@ class Catalog {
    */
   TableMetadata *CreateTable(Transaction *txn, const std::string &table_name, const Schema &schema) {
     BUSTUB_ASSERT(names_.count(table_name) == 0, "Table names should be unique!");
-    return nullptr;
+    auto table_id = next_table_oid_++;
+    TableHeap *new_table = new TableHeap(bpm_, lock_manager_, log_manager_, txn);
+    names_[table_name] = table_id;
+    auto table_meta_data = new TableMetadata(schema, table_name, static_cast<std::unique_ptr<TableHeap>>(new_table), table_id);
+    tables_[table_id] = static_cast<std::unique_ptr<TableMetadata>>(table_meta_data);
+    std::cout << "@CreateTable: table_id is " << table_id << std::endl;
+    std::cout << "@CreateTable: its mata_data is " << table_meta_data << std::endl;
+    return table_meta_data;
   }
 
   /** @return table metadata by name */
-  TableMetadata *GetTable(const std::string &table_name) { return nullptr; }
+  TableMetadata *GetTable(const std::string &table_name) { 
+    if(names_.count(table_name)==0){
+      throw std::out_of_range(table_name);
+    }
+    auto table_id = names_[table_name];
+
+    return GetTable(table_id); 
+  }
 
   /** @return table metadata by oid */
-  TableMetadata *GetTable(table_oid_t table_oid) { return nullptr; }
+  TableMetadata *GetTable(table_oid_t table_oid) { 
+    auto now_table = tables_.find(table_oid);
+    if(now_table==tables_.end()){
+      throw Exception("Can't find table id "+table_oid);
+    }
+    return now_table->second.get();
+  }
 
   /**
    * Create a new index, populate existing data of the table and return its metadata.
@@ -101,14 +121,48 @@ class Catalog {
   IndexInfo *CreateIndex(Transaction *txn, const std::string &index_name, const std::string &table_name,
                          const Schema &schema, const Schema &key_schema, const std::vector<uint32_t> &key_attrs,
                          size_t keysize) {
-    return nullptr;
+    auto index_id = next_index_oid_++;
+    auto index_meta_data = new IndexMetadata(index_name, table_name, &schema, key_attrs);
+    std::unique_ptr<Index> tree_index(new BPlusTreeIndex<KeyType, ValueType, KeyComparator>(index_meta_data, bpm_));
+    IndexInfo *index_info = 
+      new IndexInfo(key_schema, index_name, std::move(tree_index), index_id, table_name, keysize);
+    indexes_[index_id] = std::unique_ptr<IndexInfo>(index_info);
+    index_names_[table_name].emplace(index_name, index_id);
+
+    auto table_mata_data = GetTable(table_name);
+    auto table = table_mata_data->table_.get();
+    for(auto it = table->Begin(txn); it != table->End(); it++){
+      index_info->index_->InsertEntry(it->KeyFromTuple(schema, key_schema, key_attrs), it->GetRid(), txn);
+    }
+    return index_info;
   }
 
-  IndexInfo *GetIndex(const std::string &index_name, const std::string &table_name) { return nullptr; }
+  IndexInfo *GetIndex(const std::string &index_name, const std::string &table_name) { 
+    if(index_names_.count(table_name)==0){
+      throw Exception("Can't find Index named "+index_name+" in table named "+table_name);
+    }
+    index_oid_t index_id = index_names_[table_name][index_name];
+    return GetIndex(index_id);
+  }
 
-  IndexInfo *GetIndex(index_oid_t index_oid) { return nullptr; }
+  IndexInfo *GetIndex(index_oid_t index_oid) { 
+    if(indexes_.count(index_oid)==0){
+      throw Exception("Can't find index with id "+index_oid);
+    }
+    return indexes_[index_oid].get();
+  }
 
-  std::vector<IndexInfo *> GetTableIndexes(const std::string &table_name) { return std::vector<IndexInfo *>(); }
+  std::vector<IndexInfo *> GetTableIndexes(const std::string &table_name) { 
+    if(index_names_.count(table_name)==0){
+      throw Exception("Can't find Index in table named "+table_name);
+    }
+    std::vector<IndexInfo *> result;
+    auto data = index_names_[table_name];
+    for(auto it = data.begin(); it != data.end(); it++){
+      result.push_back(indexes_[it->second].get());
+    }
+    return result;
+  }
 
  private:
   [[maybe_unused]] BufferPoolManager *bpm_;
